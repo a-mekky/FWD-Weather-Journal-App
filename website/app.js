2e0a013650593b9ddeb9ea8c65988942390c3246
Abdelrahman Adel Mekky <57879478+AbdelrahmanAdel102@users.noreply.github.com>
2022-06-13 16:34:15 +0200

    fix date 'month'

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
 /* Global Variables */
 const baseUrl =
